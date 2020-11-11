e1ea2ad3ac98b06eb9d4394402713666a5d85e8d
Oxfordblue7 <457620544@qq.com>
2020-11-11 16:12:30 -0500

    Update utils.py

--- q4_tw_rd/utils.py
+++ q4_tw_rd/utils.py
@@ -1,6 +1,11 @@
 import re
 from nltk.corpus import stopwords
 import en_core_web_sm
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
 
 nlp = en_core_web_sm.load(disable=['parser', 'ner'])
 
@@ -21,3 +26,24 @@ def preprocessing(raw_text):
     # remove some specific words, like "breast", "cancer", "#"
     words_lemmatized2 = [x for x in words_lemmatized if (x not in ["#", "-", "breast", "cancer", "breastcancer"]) and (len(x) > 1)]
     return words_lemmatized2
+
+
+def visual_topTopics(model, ktopics, kwords, plotfile):
+    top_words = [[word for word, _ in model.show_topic(topic_id, topn=50)] for topic_id in range(model.num_topics)]
+    top_betas = [[beta for _, beta in model.show_topic(topic_id, topn=50)] for topic_id in range(model.num_topics)]
+
+    ncols = math.ceil(math.sqrt(ktopics))
+    nrows = math.ceil(ktopics * 1. / ncols)
+    gs = gridspec.GridSpec(nrows, ncols)
+    gs.update(wspace=0.5, hspace=0.5)
+    plt.figure(figsize=(20, 15))
+    for i in range(ktopics):
+        ax = plt.subplot(gs[i])
+        plt.barh(range(kwords), top_betas[i][:kwords], align='center', color='blue', ecolor='black')
+        ax.invert_yaxis()
+        plt.xticks(rotation=45)
+        ax.set_yticks(range(kwords))
+        ax.set_yticklabels(top_words[i][:kwords])
+        plt.title("Topic " + str(i), fontsize=14)
+
+    plt.savefig(plotfile)
